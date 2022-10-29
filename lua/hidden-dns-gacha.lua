deaf2b85faedb36364b99fec5eb943ae85465b9f
nyanshiba <31783332+nyanshiba@users.noreply.github.com>
2022-10-30 01:06:11 +0900

    輻輳時のNGN DNSへのフォールバックが機能していないのを修正
    nslookupでは不正確なので、rt.socket.dns.toip()でAレコードのみを問い合わせて計測する
    
    dns server select NUMを変更

--- lua/hidden-dns-gacha.lua
+++ lua/hidden-dns-gacha.lua
@@ -1,4 +1,3 @@
-
 -- ベンチマークに使う頻出ドメインリスト。syslogから探すとよいが、静的DNSレコードは除くべき
 -- os.difftimeがミリ秒単位で測れないので、秒単位の差がつく程度のクエリ量がよい
 fqdntbl = { 'iphone-ld.apple.com','tpop-api.twitter.com','www3.nhk.or.jp','connectivitycheck.gstatic.com','discord.com','ctldl.windowsupdate.com','imap01.au.com','proxsee.pscp.tv','i.ytimg.com','www.youtube.com','r3.o.lencr.org','config.edge.skype.com','www.bing.com','cdnjs.cloudflare.com','scrootca1.ocsp.secomtrust.net','b.st-hatena.com','star.c10r.facebook.com','cdn.jsdelivr.net','na.albtls.t.co','code.jquery.com','ipv4only.arpa' }
@@ -18,7 +17,7 @@ end
 -- クエリ時間を計測する関数
 function measure_qtime(peer_num, dns)
     -- 一時的にAレコードの問い合わせを任意のDNSに向ける。10番より前に外せないdns server selectを、後にdns server select a/aaaa/mx .が書かれている前提
-    rt.command($'dns server select 10 ${dns} edns=on a .')
+    rt.command($'dns server select 15 ${dns} edns=on a .')
     -- alias @isp11, @isp12を実行して、マルチホーム環境で一時的にDNSのパケットがPP 1, 2へルーティングされるようip route default gatewayを切り替える
     rt.command($'@isp1${peer_num}')
 
@@ -27,14 +26,14 @@ function measure_qtime(peer_num, dns)
     local t1 = os.clock()
     do
         for i, v in ipairs(fqdntbl) do
-            rt.command($'nslookup ${v}')
+            rt.socket.dns.toip(v)
         end
     end
     local t2 = os.clock()
     local qtime = os.difftime(t2, t1)
     rt.command('dns cache use on')
 
-    rt.command($'no dns server select 10')
+    rt.command($'no dns server select 15')
     -- alias @isp10を実行して、マルチホーム環境でip route default gatewayをロードバランスする設定に戻す
     rt.command('@isp10')
 
@@ -63,35 +62,38 @@ function set_reverse_nat(peer_num, dummy_dns, fast_dns)
     end
 end
 
--- 先にAAAAレコード用のDNSサーバを選ぶ
+-- AAAAレコードの問い合わせに使うNGNのDNSサーバを選ぶ
 for i, v in ipairs(get_dhcpv6_dns()) do
-    qtime = measure_qtime(0, v)
-    -- 初期値を優先DNSサーバとし、RTTが同等以下であれば代替を選ぶ
-    if i == 1 or qtime <= fast_qtime_v6 then
+    local qtime = measure_qtime(0, v)
+    -- 初期値を優先DNSサーバとし、RTTがより小さければ代替を選ぶ
+    if i == 1 or qtime < fast_qtime_v6 then
         fast_qtime_v6 = qtime
         fast_dns_v6 = v
     end
 end
-rt.command($'dns server select 20 ${fast_dns_v6} edns=on aaaa .')
+rt.command($'dns server select 25 ${fast_dns_v6} edns=on aaaa .')
 
--- Aレコード用のDNSサーバを選ぶ
+-- Aレコードの問い合わせに使うISPのDNSサーバを選ぶ
 for i = 1, 2 do
     -- 計測・設定更新に邪魔な以前の逆NAT設定を削除
     no_reverse_nat(i)
 
-    local fast_qtime, fast_dns, qtime = fast_qtime_v6, fast_dns_v6
     -- IPv4 DNSをベンチマーク
     for j, v in ipairs(get_ipcp_dns(i)) do
-        qtime = measure_qtime(i, v)
-        -- AAAAレコード用のDNSサーバを初期値とし、同等以下のRTTならIPv4 DNSを選ぶ(PPPoEの輻輳・パケロス増加時を想定)
-        if qtime <= fast_qtime then
+        local qtime = measure_qtime(i, v)
+        -- 初期値を優先DNSサーバとし、RTTが同等以下であれば代替を選ぶ
+        if j == 1 or qtime <= fast_qtime then
             fast_qtime = qtime
             fast_dns = v
         end
     end
 
-    -- 逆NATにより1.1.1.1への問い合わせが最もRTTが優れたISPのDNSに向くよう、静的NATエントリを追加する例
-    if fast_dns ~= fast_dns_v6 then
+    -- NGNのDNSの方が速ければAレコードの問い合わせに使う(PPPoEの輻輳・パケロス増加時を想定)
+    if fast_qtime_v6 < fast_qtime then
+        rt.command($'dns server select ${25 + i} ${fast_dns_v6} edns=on a .')
+    else
+        -- 逆NATにより1.1.1.1への問い合わせが最もRTTが優れたISPのDNSに向くよう、静的NATエントリを追加する例
         set_reverse_nat(i, '1.1.1.1', fast_dns)
+        rt.command($'dns server select ${25 + i} 1.1.1.1 edns=on a . restrict pp ${i}')
     end
 end
